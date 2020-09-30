a1221a9c4490334ba75452a382e1c9adead22764
Shimul Chowdhury <shimul.chowdhury192@gmail.com>
2020-10-01 01:56:36 +0600

    setup tests

--- bizli/bizli.py
+++ bizli/bizli.py
@@ -1,9 +1,10 @@
 import typer
-
+import os
 
 app = typer.Typer()
 
 
 @app.command()
 def hello(name: str):
+    cwd = os.getcwd()
     typer.echo(f'{name}')
--- tests/test_bizli.py
+++ tests/test_bizli.py
@@ -1,5 +1,16 @@
+from typer.testing import CliRunner
+
 from bizli import __version__
+from bizli.bizli import app
+
+runner = CliRunner()
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_app():
+    result = runner.invoke(app, ["asd"])
+    assert result.exit_code == 0
+    assert "asd" in result.stdout
