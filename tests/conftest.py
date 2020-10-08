f52854f2eb6737547379f4131228f7cfa68c17c9
Shimul Chowdhury <shimul.chowdhury192@gmail.com>
2020-10-08 09:46:46 +0600

    create playground dir if not exists

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,9 @@ from .settings import *
 def pytest_sessionstart(session):
 
     # all operation should be in playground
+    if not os.path.exists(PLAYGROUND_DIR):
+        os.mkdir(PLAYGROUND_DIR)
+
     os.chdir(PLAYGROUND_DIR)
 
     # delete migrations dir if exists
